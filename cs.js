5119f7892bc45452bc13e43e3b9001a464074a6a
Michael Flynn <mpflynn8860@eagle.fgcu.edu>
2016-12-08 15:14:10 -0700

    first commit

---
+++ cs.js
@@ -0,0 +1,90 @@
+var userInput  = document.querySelector(".input")
+var guess = document.getElementById("guess")
+var guessnumber1 = document.querySelector (".guessnumber")
+var minNum = 1
+var maxNum = 100
+var computerNumber = Math.ceil(Math.random() * maxNum * minNum)
+console.log (computerNumber)
+var response1 = document.querySelector(".response")
+var clear = document.querySelector("#clear")
+var reset = document.querySelector("#resetgame")
+var min = document.querySelector("#min")
+var max = document.querySelector("#max")
+var minimum = 0
+var maximum = 100
+
+
+
+guess.addEventListener("click", function() {
+  guessnumber1.innerText = userInput.value
+  checkFields()
+})
+
+
+function checkFields(){
+  var userNumber = userInput.value
+  var guess = parseInt(userNumber)
+if (guess > computerNumber && guess < maximum) {
+  response1.innerText = "That is too high"
+} else if (guess < computerNumber && guess > minimum){
+  response1.innerText = "That is too low"
+} else if (computerNumber === guess){
+  response1.innerText = "BOOM!"
+} else {
+  response1.innerText = "Please enter a valid number"
+}
+}
+
+clear.addEventListener("click", function () {
+  userInput.value = ""
+})
+
+reset.addEventListener("click", function () {
+  userInput.value = ""
+  guessnumber1.innerText = ""
+  response1.innerText = ""
+})
+
+userInput.addEventListener("keyup", function () {
+  clear.disabled = false;
+  reset.disabled = false;
+}
+)
+
+
+
+
+
+
+
+    // function outOfRange (){
+    // else (userInput > maxNum || userInput < minNum){
+    //   alert("OUT OF RANGE")
+
+
+
+
+
+
+
+
+
+
+  // * The application should display an error if
+  // the guess is outside of the range of possible answers.
+
+
+
+
+
+
+//   if (userNumber < computerNumber){
+//     response1.innerText = "That is too low"
+//   } else if (computerNumber > userNumber){
+//     response1.innerText = "That is too high"
+//   } else if (computerNumber === userNumber){
+//     response1.innerText = "BOOM!"
+//   }  else (isNaN(parse)){
+//     response1.innerText = "Not A Number"
+//   }
+// }
